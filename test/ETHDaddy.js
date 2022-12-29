017a7d0032af5e1b705f6e75dd9208a9b1b25de1
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-29 13:27:35 +0530

    sets and validates owner

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -6,20 +6,34 @@ const tokens = (token) => {
 }
 
 describe('ETHDaddy', () => {
-  let ethDaddy
+  let ethDaddy, deployer, owner
+  const CONTRACT_NAME = 'ETHDaddy'
+  const NAME = 'ETH Daddy'
+  const SYMBOL = 'ETHD'
 
   beforeEach(async () => {
-    const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
-    ethDaddy = await ETHDaddy.deploy('ETH Daddy', 'ETHD')
+    // Set-Up Accounts
+    ;[deployer, owner] = await ethers.getSigners()
+
+    // Deploy Contract
+    const ETHDaddy = await ethers.getContractFactory(CONTRACT_NAME)
+    ethDaddy = await ETHDaddy.deploy(NAME, SYMBOL)
   })
 
-  it('should have a name', async () => {
-    const result = await ethDaddy.name()
-    expect(result).to.equal('ETH Daddy')
-  })
+  describe('Deployment', () => {
+    it('should have a name', async () => {
+      const result = await ethDaddy.name()
+      expect(result).to.equal('ETH Daddy')
+    })
 
-  it('should have a symbol', async () => {
-    const result = await ethDaddy.symbol()
-    expect(result).to.equal('ETHD')
+    it('should have a symbol', async () => {
+      const result = await ethDaddy.symbol()
+      expect(result).to.equal('ETHD')
+    })
+
+    it('should set the owner', async () => {
+      const result = await ethDaddy.owner()
+      expect(result).to.equal(deployer.address)
+    })
   })
 })
