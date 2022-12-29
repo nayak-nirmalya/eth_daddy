c01b8a6b23727a54cc5ce6fd7cab43dd4cf06ee0
Nirmalya Nayak <n.nayak263@gmail.com>
2022-12-29 12:19:16 +0530

    smoke test

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,15 @@
-const { expect } = require("chai")
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe("ETHDaddy", () => {
-
+describe('ETHDaddy', () => {
+  it('has a name', async () => {
+    const ETHDaddy = await ethers.getContractFactory('ETHDaddy')
+    let ethDaddy = await ETHDaddy.deploy()
+    const result = await ethDaddy.name()
+    expect(result).to.equal('ETH Daddy')
+  })
 })
